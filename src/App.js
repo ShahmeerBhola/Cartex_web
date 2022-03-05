23a9d283bba821a33f32a6fcc76ca6c06065e63a
bhola-tech <affanmuhammad623@gmail.com>
2022-03-05 06:45:23 +0500

    terms and profile

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './styles/tailwind.css';
 import './styles/custom.css';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Terms from './pages/Terms';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Abouts/>} />
         <Route path="privacy" element={<PrivacyPolicy/>} />
+        <Route path="terms" element={<Terms/>} />
+        <Route path="profile" element={<Profile/>} />
       </Routes>
       </BrowserRouter>
     </div>
