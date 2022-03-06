b6d583e8e47d34038815346126926d7c948fb0b0
bhola-tech <affanmuhammad623@gmail.com>
2022-03-07 01:48:36 +0500

    profile completed successfully

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -13,7 +13,7 @@ function Footer() {
       <div className='bg-white py-5 w-full'>
           <img className='mx-auto justify-center w-[280px] md:w-[302px] h-[48px]' src={Cartex} alt="Cartex logo"></img>
           <br/>
-          <div className='flex flex-col justify-center md:flex-row md:justify-evenly w-full px-2 md:px-0 md:w-11/12  lg:w-10/12 mx-auto pb-4'>
+          <div className='flex flex-col justify-center md:flex-row md:justify-evenly w-full px-4 md:px-0 md:w-11/12  lg:w-10/12 mx-auto pb-4'>
               <div className='flex flex-col w-full md:mx-0 md:w-1/2 justify-between py-2 gap-4 '>
                   <h1 className=' text-2xl font-bold  '>Informations</h1>
                   <a className='text-xl ' >Terms And Conditions</a>
--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Card from "../component/UI/Card"
 import profilepic from "../Images/Profile/profileimg.png"
-
+import CameraAltOutlinedIcon from '@mui/icons-material/CameraAltOutlined';
 
 const EditProfile = () => {
   return (
@@ -10,29 +10,32 @@ const EditProfile = () => {
         <div className='flex flex-col md:flex-row mx-auto w-10/12 md:w-11/12 gap-2 md:gap-2'>
             <Card css=" w-full lg:w-3/4 min-h-[348px]">
                   <div className='flex flex-col pt-4 justify-center items-center md:justify-start md:items-start '>
-                      <div>
-                        <img className='rounded-full p-0' src={profilepic} alt='profile pic' height="180px" width="180px" ></img>
-                        {/* <img></img> */}
+                      <div className='flex items-center justify-center relative'>
+                        <img className='rounded-full p-0 brightness-50' src={profilepic} alt='profile pic' height="180px" width="180px"  ></img>
+                        <div className='h-[180px] w-[180px] absolute  left-20 top-20 md:left-[65px] md:top-[55px]  '> <CameraAltOutlinedIcon className='text-white'/></div>
                       </div>
-                <div className='text-[17px] text-[#1A7DC1] font-bold py-3 '>Change Photo</div>
-                <div className='text-[17px] text-[#FF0000] font-bold pb-5'>Remove Photo</div>
+                <div className='text-[17px] text-[#1A7DC1] font-bold py-3 pl-3'>Change Photo</div>
+                <div className='text-[17px] text-[#FF0000] font-bold pb-5 pl-3'>Remove Photo</div>
             </div>
             <div className='flex flex-col md:px-12 w-full gap-4'>
             
-                      <div className='rounded-lg border-[#CCCCCC]  border-2 w-full md:w-3/4'>
-                          <div className='text-[15x] text-[#F47521] font-bold'>Name</div>
-                          <p className='font-bold text-2xl' contentEditable={true}>Jane Doe</p>
+                      <div className='rounded-lg border-[#CCCCCC]  border-2 w-full md:w-3/4 px-2'>
+                          <div className='text-[15px] text-[#F47521] font-bold'>Name</div>
+                          <p className='font-bold text-2xl border-[#fff]' contentEditable={true}>Jane Doe</p>
                       </div>
-                      <div className='rounded-lg border-[#CCCCCC]  border-2 w-full md:w-3/4'>
-                          <div className='text-[15x] text-[#F47521] font-bold'>Mobile No.</div>
-                          <p className='font-bold text-2xl' contentEditable={true}>+1-234-567-890</p>
+                      <div className='rounded-lg border-[#CCCCCC]  border-2 w-full md:w-3/4 px-2 py-2'>
+                          <div className='text-[15px] text-[#F47521] font-bold'>Mobile No.</div>
+              <p className='font-bold text-2xl' contentEditable={true}><span className='border-[1px] rounded-lg border-[#F47521] text-sm p-1 ' ><label className='text-[#F47521]' for="phonecode"><select  id="phonecode">
+                <option value="+1">+1</option>
+              
+              </select></label></span>  234-567-890</p>
                       </div>
-                      <div className='rounded-lg border-[#CCCCCC]  border-2 w-full md:w-3/4'>
-                          <div className='text-[15x] text-[#F47521] font-bold'>Email</div>
+                      <div className='rounded-lg border-[#CCCCCC]  border-2 w-full md:w-3/4 px-2'>
+                          <div className='text-[15px] text-[#F47521] font-bold'>Email</div>
                           <p className='font-bold text-2xl' contentEditable={true}>janedoe@gmail.com</p>
                       </div>
                       <div className='flex flex-col justify-center items-center gap-2 md:w-3/4'>
-                      <div className='bg-[#F47521] rounded-3xl text-center text-white w-[340px] h-[48px] flex justify-center items-center'>Save Changes</div>
+                      <div className='bg-[#F47521] rounded-3xl text-center text-white w-[280px] md:w-[340px] h-[48px] flex justify-center items-center'>Save Changes</div>
                 <div className=' rounded-3xl text-center text-[#F47521] w-[170px] px-2 py-3'>Discard Changes</div>
                </div>       
             </div>
