18ba69e40a8d45f83d9502ba67768997267a3ae7
ShahmeerBhola <shahmir.shamim1@gmail.com>
2022-03-19 18:15:31 +0500

    profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import profilepic from "../Images/Profile/profileimg.png"
 import customerService from "../Images/Profile/customer-service.png"
 import Card from '../component/UI/Card'
 import { useNavigate } from 'react-router-dom'
+
 // class="flex flex-col md:flex-row mx-auto w-10/12 md:w-11/12 gap-2 md:gap-2"
 function Profile() {
     const navigate=useNavigate()
@@ -11,6 +12,7 @@ function Profile() {
         <div className='flex flex-col md:flex-row mx-auto w-10/12 md:w-11/12 gap-2 md:gap-2'>
             <Card css=" w-full md:w-3/4 min-h-[348px]">
             <div className='flex flex-col pt-4 justify-center items-center'>
+                
                 <img className='rounded-full p-0 ' src={localStorage.getItem('image')} alt='profile pic' height="180px" width="180px" ></img>
                 <div className='bg-[#F47521] m-3 p-2 px-5 rounded-3xl text-center text-white w-[170px] cursor-pointer'  onClick={()=>navigate('/profile/editprofile')}>Edit profile</div>
                 
